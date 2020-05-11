421412e2769eccdae408329a532246f765e43921
Łukasz Jóźwik <jozwiklukasz@outlook.com>
2020-05-11 22:03:50 +0200

    Fix issues

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,9 +10,11 @@ class Quiz extends React.Component {
     }
 
     render() {
-        return <div class="QuizQuestion">
+        return <div>
+            <div className="QuizQuestion">
                 {quizData.quiz_questions[0].instruction_text}
-            </div>;
+            </div>
+        </div>;
     }
 }
 
